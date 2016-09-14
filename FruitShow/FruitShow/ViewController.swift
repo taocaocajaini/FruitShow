f444ab44bec4dc5304dc98a5b9d3372f7b58a131
apple <apple@appledemacbook-air.local>
2016-09-14 10:21:47 +0800

    初始化版本

---
+++ FruitShow/FruitShow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FruitShow
+//
+//  Created by Apple on 16/9/14.
+//  Copyright © 2016年 Tc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
